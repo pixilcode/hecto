0dcd1b79a015e79c9d482653b871eff718907c77
Brendon Bown <brendon.bown@gmail.com>
2021-11-20 09:58:43 -0700

    Switched from reading bytes to termion keys

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-
-                if c.is_control() {
-                    println!("{:#b}\r", b);
-                } else {
-                    println!("{:#b} ({})\r", b, c);
-                }
-
-                println!("{}", c);
-                if b == to_control_byte('q') {
-                    break;
-                }
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => 
+                    if c.is_control() {
+                        println!("{:?}\r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    },
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
             },
             Err(err) => die(err)
         }
     }
-}
-
-fn to_control_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
 }
 
 fn die(e: io::Error) {
