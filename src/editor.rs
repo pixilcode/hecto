95e5566f3456bb3c4ae643184392ce364b0b1512
Brendon Bown <brendon.bown@gmail.com>
2021-11-20 23:16:21 -0700

    Refresh the screen in every loop

--- src/editor.rs
+++ src/editor.rs
@@ -1,27 +1,42 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use std::default::Default;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+	should_quit: bool,
+}
 
 impl Editor {
-	pub fn run(&self) {
+	pub fn run(&mut self) {
 		let _stdout = stdout().into_raw_mode().unwrap();
 
 		loop {
+			if let Err(error) = self.refresh_screen() {
+				die(error);
+			}
+
+			if self.should_quit {
+				break;
+			}
+
 			if let Err(error) = self.process_keypress() {
 				die(error);
 			}
 		}
 	}
 
-	fn process_keypress(&self) -> Result<(), io::Error> {
+	fn refresh_screen(&self) -> Result<(), io::Error> {
+		print!("{}", termion::clear::All);
+		stdout().flush()
+	}
+
+	fn process_keypress(&mut self) -> Result<(), io::Error> {
 		let pressed_key = read_key()?;
 
 		match pressed_key {
-			Key::Ctrl('q') => panic!("Program end"),
+			Key::Ctrl('q') => self.should_quit = true,
 			_ => ()
 		}
 
@@ -31,7 +46,9 @@ impl Editor {
 
 impl Default for Editor {
 	fn default() -> Self {
-		Self {}
+		Self {
+			should_quit: false,
+		}
 	}
 }
 
